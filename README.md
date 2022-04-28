c07364fa951d06db2b9a5ecdab69256e3313b1e2
Ignacio Ainol <iggnaxios@gmail.com>
2022-04-27 20:48:35 -0400

    fix issues to deploy

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # MERN TEST BLOG
 
 ## Demo Website
+
+Comando para hacer correr la APP
+```
+npm run build
+npm start
+```
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import path from 'path';
 import postRouter from './routes/posts.js';
 
 dotenv.config();
+
 mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log('connected to db')
 }).catch(err => {
@@ -19,6 +21,16 @@ app.use(express.urlencoded({ extended: true }));
 
 //routes
 app.use('/api/posts', postRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
